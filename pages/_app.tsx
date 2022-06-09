91a5411bc9b620b3dd2c95dd7e037a7b822b714e
Steven <ssteven075@gmail.com>
2022-06-09 17:30:45 +0700

    Set up dependencies and Chakra Provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+import theme from "../theme";
+import "../theme/styles.css";
+import { UserContextProvider } from "../components/context/UserContext";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<ChakraProvider theme={theme}>
+			<UserContextProvider>
+				<Component {...pageProps} />
+			</UserContextProvider>
+		</ChakraProvider>
+	);
 }
 
-export default MyApp
+export default MyApp;
