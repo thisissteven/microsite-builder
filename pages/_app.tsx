0607d91713cf6db21e0ee3491078eb09c22331c5
Steven <ssteven075@gmail.com>
2022-06-09 19:41:57 +0700

    Add Navbar to App

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,20 @@
 import type { AppProps } from "next/app";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, Container, VStack } from "@chakra-ui/react";
 import theme from "../theme";
 import "../theme/styles.css";
 import { UserContextProvider } from "../components/context/UserContext";
+import Navbar from "../components/elements/Navbar";
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<ChakraProvider theme={theme}>
 			<UserContextProvider>
-				<Component {...pageProps} />
+				<Container h="100vh" maxW="container.xl">
+					<VStack p={{ base: 2, sm: 8 }} pt={8} spacing={8} h="full">
+						<Navbar />
+						<Component {...pageProps} />
+					</VStack>
+				</Container>
 			</UserContextProvider>
 		</ChakraProvider>
 	);
