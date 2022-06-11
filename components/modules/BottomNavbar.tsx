135a0134715d8860f2141d664f515cda3100d26c
Steven <ssteven075@gmail.com>
2022-06-11 16:32:10 +0700

    Fix button styles

--- components/modules/BottomNavbar.tsx
+++ components/modules/BottomNavbar.tsx
@@ -1,30 +1,34 @@
 import { Box, Button, Flex, Heading, HStack, useColorModeValue, VStack } from "@chakra-ui/react";
 import Link from "next/link";
-import { useRouter } from "next/router";
-import React, { MouseEventHandler } from "react";
+import React from "react";
 import { useUserContext } from "../context/UserContext";
-import { AnimatePresence, motion } from "framer-motion";
 
 const BottomNavbar = () => {
 	const { user } = useUserContext();
 
-	const { pathname } = useRouter();
-
-	const listBg = useColorModeValue("#E0E0E0", "#424242");
 	const buttonColor = useColorModeValue("#323232", "#EEEEEE");
-	const MotionFlex = motion(Flex);
 
 	if (user === null) return <></>;
 
 	return (
 		<HStack position="absolute" bottom={0} right={0} py={2} px={4} rounded="lg" spacing={4}>
-			<Button variant="link">
+			<Button
+				variant="link"
+				color={buttonColor}
+				_active={{ opacity: 0.6 }}
+				_hover={{ textDecoration: "underline", opacity: 0.8 }}
+			>
 				<Link href="/microsite">
 					<a>Microsite</a>
 				</Link>
 			</Button>
 
-			<Button variant="link">
+			<Button
+				variant="link"
+				color={buttonColor}
+				_active={{ opacity: 0.6 }}
+				_hover={{ textDecoration: "underline", opacity: 0.8 }}
+			>
 				<Link href="/links">
 					<a>Your links</a>
 				</Link>
