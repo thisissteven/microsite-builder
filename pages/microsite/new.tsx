d11b4e7db69ac3841f5f122d276cacf84dd2fa0b
Steven <ssteven075@gmail.com>
2022-06-13 12:13:00 +0700

    Initial design for microsite page

--- pages/microsite/new.tsx
+++ pages/microsite/new.tsx
@@ -1,17 +1,78 @@
-import { Box, IconButton, Container, Heading, HStack, Tooltip, useColorModeValue, VStack } from "@chakra-ui/react";
+import {
+	Box,
+	IconButton,
+	Container,
+	Heading,
+	HStack,
+	Text,
+	useColorModeValue,
+	VStack,
+	Button,
+	Progress,
+} from "@chakra-ui/react";
 import type { NextPage } from "next";
-import Link from "next/link";
-import { useUserContext } from "../../components/context/UserContext";
 import Layout from "../../components/elements/Layout";
-import { MdOutlineAdd } from "react-icons/md";
+import { useEffect, useState } from "react";
+import { useUserContext } from "../../components/context/UserContext";
+import { AnimatePresence, motion } from "framer-motion";
+
+const progressTitle = ["1. Select Template", "2. Name Your Microsite", "3. Congratulations! Your Microsite is done!"];
 
 const NewMicrosite: NextPage = () => {
-	const { user } = useUserContext();
+	const { token } = useUserContext();
+	const [value, setValue] = useState(0);
+	const [progress, setProgress] = useState(1);
+	const [title, setTitle] = useState("");
+
+	const updateProgress = () => {
+		const percentage = 33.3;
+		setValue(progress * percentage);
+		setTitle(progressTitle[progress - 1]);
+	};
+
+	useEffect(() => {
+		updateProgress();
+	}, [progress]);
+
+	const MotionText = motion(Text);
 
 	return (
 		<Layout>
-			<VStack minH="70vh" spacing={8} alignItems={{ base: "flex-start", sm: "center" }} h="full" w="full">
-				<Heading size={{ base: "lg", sm: "xl" }}>Welcome</Heading>
+			<VStack minH="70vh" spacing={8} alignItems="flex-start" h="full" w="full" mb={4}>
+				<Heading size={{ base: "lg", sm: "xl" }}>Create New Microsite</Heading>
+				<AnimatePresence exitBeforeEnter initial={false}>
+					<MotionText
+						fontSize="lg"
+						initial={{ x: -100, opacity: 0 }}
+						animate={{ x: 0, opacity: 1, transition: { duration: 0.3 } }}
+						key={title}
+					>
+						{title}
+					</MotionText>
+				</AnimatePresence>
+				<Progress
+					sx={{
+						"& > div:first-of-type": {
+							transitionProperty: "width",
+						},
+					}}
+					value={value}
+					size="xs"
+					colorScheme="green"
+					w="full"
+					rounded="md"
+				/>
+				<VStack h="60vh" w="full" spacing={4} bg="whiteAlpha.200" rounded="md" overflow="hidden" p={4}>
+					<Box h="full" w="full" bg="whiteAlpha.300" rounded="sm"></Box>
+					{progress !== 3 ? (
+						<HStack w="full" justifyContent="flex-end">
+							{1 < progress && <Button onClick={() => setProgress(progress - 1)}>Back</Button>}
+							<Button onClick={() => setProgress(progress + 1)}>Next</Button>
+						</HStack>
+					) : (
+						<Button alignSelf="flex-end" isLoading={true}></Button>
+					)}
+				</VStack>
 			</VStack>
 		</Layout>
 	);
