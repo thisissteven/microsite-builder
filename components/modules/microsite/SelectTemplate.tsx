c5b617cbfa7b2d9b28cc0e48764640d1ea4d4bf7
Steven <ssteven075@gmail.com>
2022-06-13 12:14:06 +0700

    Initialize react components

---
+++ components/modules/microsite/SelectTemplate.tsx
@@ -0,0 +1,13 @@
+import { Box, Button, Text, Heading, HStack, useColorModeValue, VStack, IconButton } from "@chakra-ui/react";
+import Link from "next/link";
+import React from "react";
+import PoweredByVercel from "powered-by-vercel";
+import { AiOutlineGithub } from "react-icons/ai";
+
+const SelectTemplate = () => {
+	const textColor = useColorModeValue("red.400", "red.300");
+
+	return <HStack spacing={6} rounded="lg" w="full" justifyContent="flex-end"></HStack>;
+};
+
+export default SelectTemplate;
