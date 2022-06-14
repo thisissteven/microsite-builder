e7ac87422b15ff1af635769b74f8f65cb60bbd4f
Steven <ssteven075@gmail.com>
2022-06-14 21:33:40 +0700

    Update styling

--- components/modules/microsite/SelectTemplate.tsx
+++ components/modules/microsite/SelectTemplate.tsx
@@ -1,35 +1,126 @@
-import { Box, Button, Text, Heading, HStack, useColorModeValue, VStack, IconButton } from "@chakra-ui/react";
+import {
+	Box,
+	Button,
+	Text,
+	Heading,
+	HStack,
+	useColorModeValue,
+	VStack,
+	IconButton,
+	FormControl,
+	FormLabel,
+	Input,
+	Flex,
+	Image,
+	Icon,
+	Tooltip,
+} from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
 import { SwatchesPicker } from "react-color";
+import UserInput from "../../elements/UserInput";
+import { AiOutlineEdit } from "react-icons/ai";
+import { useMicrositeContext } from "../../context/MicrositeContext";
 
 const SelectTemplate = () => {
 	const textColor = useColorModeValue("red.400", "red.300");
 
-	const [background, setBackground] = useState("#fff");
+	const { register, handleSubmit, reset, getValues, isSubmitting, setBackground, background } = useMicrositeContext();
+
+	const displayColor = useColorModeValue("blackAlpha.200", "whiteAlpha.200");
 
 	const handleChangeComplete = (color: any, event: any) => {
 		setBackground(color.hex);
 	};
 
+	const onSubmit = async (data: any) => {
+		console.log(data);
+		console.log(background);
+	};
+
 	return (
-		<HStack justifyContent="center" gap={2} spacing={0} rounded="lg" w="full" flexWrap="wrap">
-			<VStack alignItems="flex-start" h="full" p={{ base: 2, sm: 4 }} spacing={4}>
+		<HStack
+			justifyContent="center"
+			overflow="hidden"
+			gap={2}
+			spacing={0}
+			rounded="lg"
+			w="full"
+			flexWrap={{ base: "wrap", md: "nowrap" }}
+		>
+			<VStack alignItems="flex-start" p={{ base: 2, sm: 4 }} spacing={4}>
 				<Text fontSize="lg" fontWeight="medium" w="full">
 					Pick your theme
 				</Text>
 				<SwatchesPicker color={background} onChangeComplete={handleChangeComplete} />
 			</VStack>
-			<VStack alignItems="flex-start" h="full" p={{ base: 2, sm: 4 }} spacing={4}>
-				<Text fontSize="lg" fontWeight="medium" w="full">
-					Some description
-				</Text>
-				{/* <SwatchesPicker color={background} onChangeComplete={handleChangeComplete} /> */}
-			</VStack>
-			<VStack alignItems="flex-start" h="full" p={{ base: 2, sm: 4 }} spacing={4}>
-				<Text fontSize="lg" fontWeight="medium" w="full">
-					Social media links
-				</Text>
-				{/* <SwatchesPicker color={background} onChangeComplete={handleChangeComplete} /> */}
+			<VStack alignItems="flex-start" p={{ base: 2, sm: 4 }} spacing={4}>
+				<Box
+					as="form"
+					display="flex"
+					alignItems="flex-end"
+					gap={{ base: 0, sm: 2 }}
+					flexDir={{ base: "column", sm: "row" }}
+					onSubmit={handleSubmit(onSubmit)}
+				>
+					<FormControl minW={{ base: "240px", sm: "280px" }} px={{ base: 8, sm: 4, md: 0 }}>
+						<VStack spacing={4} alignItems="flex-start">
+							<Box w={{ base: "auto", sm: "full" }}>
+								<FormLabel htmlFor="" mb={2} fontSize={{ base: "sm", sm: "md" }}>
+									Title
+								</FormLabel>
+								<UserInput
+									placeholder="Microsite Title"
+									isInvalid={getValues().title === ""}
+									register={register}
+									name="displayName"
+									maxLength={18}
+								/>
+							</Box>
+							<Box w={{ base: "auto", sm: "full" }}>
+								<FormLabel htmlFor="" mb={2} fontSize={{ base: "sm", sm: "md" }}>
+									Description
+								</FormLabel>
+								<UserInput placeholder="Description" register={register} name="description" maxLength={72} />
+							</Box>
+							<Box w={{ base: "auto", sm: "full" }} pt={4}>
+								<HStack>
+									<VStack alignItems="flex-start" w="full">
+										<FormLabel htmlFor="" mb={{ base: 0, sm: 2 }} fontSize={{ base: "sm", sm: "md" }}>
+											Display Picture
+										</FormLabel>
+										<Text fontSize="sm">No Image Uploaded.</Text>
+									</VStack>
+									<VStack alignItems="flex-end">
+										<Box
+											position="relative"
+											bg={displayColor}
+											w={{ base: 12, sm: 16 }}
+											h={{ base: 12, sm: 16 }}
+											rounded="lg"
+										>
+											<Tooltip label="Upload Image" aria-label="upload">
+												<IconButton
+													type="submit"
+													position="absolute"
+													aria-label="edit profile"
+													icon={<AiOutlineEdit />}
+													top="50%"
+													left="50%"
+													transform="auto"
+													translateX="-50%"
+													translateY="-50%"
+													bg="transparent"
+													_hover={{ bg: "transparent", opacity: 0.8 }}
+													_active={{ bg: "transparent" }}
+												/>
+											</Tooltip>
+										</Box>
+									</VStack>
+								</HStack>
+							</Box>
+						</VStack>
+					</FormControl>
+				</Box>
 			</VStack>
 		</HStack>
 	);
--- pages/microsite/new.tsx
+++ pages/microsite/new.tsx
@@ -16,8 +16,14 @@ import { useEffect, useState } from "react";
 import { useUserContext } from "../../components/context/UserContext";
 import { AnimatePresence, motion } from "framer-motion";
 import SelectTemplate from "../../components/modules/microsite/SelectTemplate";
+import { MicrositeContextProvider } from "../../components/context/MicrositeContext";
 
-const progressTitle = ["1. Select Template", "2. Name Your Microsite", "3. Congratulations! Your Microsite is done!"];
+const progressTitle = [
+	"1. Select Template",
+	"2. Pick a Style",
+	"3. Name Your Microsite",
+	"4. Congratulations! Your Microsite is done!",
+];
 
 const NewMicrosite: NextPage = () => {
 	const { token } = useUserContext();
@@ -26,7 +32,7 @@ const NewMicrosite: NextPage = () => {
 	const [title, setTitle] = useState("");
 
 	const updateProgress = () => {
-		const percentage = 33.3;
+		const percentage = 25;
 		setValue(progress * percentage);
 		setTitle(progressTitle[progress - 1]);
 	};
@@ -36,60 +42,62 @@ const NewMicrosite: NextPage = () => {
 	}, [progress]);
 
 	const MotionText = motion(Text);
-	const progressBarColor = useColorModeValue("blackAlpha.300", "whiteAlpha.200");
-	const contentBg = useColorModeValue("blackAlpha.200", "whiteAlpha.100");
+	const progressBarColor = useColorModeValue("blackAlpha.200", "whiteAlpha.200");
+	const contentBg = useColorModeValue("#EEEEEE", "#323232");
 
 	return (
-		<Layout>
-			<VStack minH="70vh" spacing={8} alignItems="flex-start" h="full" w="full" mb={4}>
-				<Heading size={{ base: "lg", sm: "xl" }}>Create New Microsite</Heading>
-				<AnimatePresence exitBeforeEnter initial={false}>
-					<MotionText
-						fontSize="lg"
-						initial={{ x: -100, opacity: 0 }}
-						animate={{ x: 0, opacity: 1, transition: { duration: 0.3 } }}
-						key={title}
+		<MicrositeContextProvider>
+			<Layout>
+				<VStack minH="70vh" spacing={8} alignItems="flex-start" h="full" w="full" mb={4}>
+					<Heading size={{ base: "lg", sm: "xl" }}>Create New Microsite</Heading>
+					<AnimatePresence exitBeforeEnter initial={false}>
+						<MotionText
+							fontSize="lg"
+							initial={{ x: -100, opacity: 0 }}
+							animate={{ x: 0, opacity: 1, transition: { duration: 0.3 } }}
+							key={title}
+						>
+							{title}
+						</MotionText>
+					</AnimatePresence>
+					<Progress
+						sx={{
+							"& > div:first-of-type": {
+								transitionProperty: "width",
+							},
+						}}
+						value={value}
+						size="xs"
+						bg={progressBarColor}
+						colorScheme={"green"}
+						w="full"
+						rounded="md"
+					/>
+					<VStack
+						h="full"
+						w="full"
+						spacing={{ base: 2, sm: 4 }}
+						bg={progressBarColor}
+						rounded="md"
+						overflow="hidden"
+						py={{ base: 2, sm: 4 }}
+						px={{ base: 2, sm: 4 }}
 					>
-						{title}
-					</MotionText>
-				</AnimatePresence>
-				<Progress
-					sx={{
-						"& > div:first-of-type": {
-							transitionProperty: "width",
-						},
-					}}
-					value={value}
-					size="xs"
-					bg={progressBarColor}
-					colorScheme={"green"}
-					w="full"
-					rounded="md"
-				/>
-				<VStack
-					h="68vh"
-					w="full"
-					spacing={4}
-					bg={progressBarColor}
-					rounded="md"
-					overflow="hidden"
-					py={4}
-					px={{ base: 2, sm: 4 }}
-				>
-					<Box h="full" w="full" rounded="sm" className="scrollbar" py={4}>
-						<SelectTemplate />
-					</Box>
-					{progress !== 3 ? (
-						<HStack w="full" justifyContent="flex-end">
-							{1 < progress && <Button onClick={() => setProgress(progress - 1)}>Back</Button>}
-							<Button onClick={() => setProgress(progress + 1)}>Next</Button>
-						</HStack>
-					) : (
-						<Button alignSelf="flex-end" isLoading={true}></Button>
-					)}
+						<Box h="full" w="full" bg={contentBg} rounded="sm" className="scrollbar" py={4}>
+							<SelectTemplate />
+						</Box>
+						{progress !== 4 ? (
+							<HStack w="full" justifyContent="flex-end">
+								{1 < progress && <Button onClick={() => setProgress(progress - 1)}>Back</Button>}
+								<Button onClick={() => setProgress(progress + 1)}>{progress !== 3 ? "Next" : "Finish"}</Button>
+							</HStack>
+						) : (
+							<Button alignSelf="flex-end" isLoading={true}></Button>
+						)}
+					</VStack>
 				</VStack>
-			</VStack>
-		</Layout>
+			</Layout>
+		</MicrositeContextProvider>
 	);
 };
 
